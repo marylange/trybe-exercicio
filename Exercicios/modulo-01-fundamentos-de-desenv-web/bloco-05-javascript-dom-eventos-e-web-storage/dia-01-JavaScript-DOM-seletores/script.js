c2b6606a44ea7b1484590c5cd693468220d5aa94
Marylange Leal <marylangess@gmail.com>
2022-02-10 12:12:26 -0300

    atualizando repositório bloco 5 dia 1

--- Exercicios/modulo-01-fundamentos-de-desenv-web/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-JavaScript-DOM-seletores/script.js
+++ Exercicios/modulo-01-fundamentos-de-desenv-web/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-JavaScript-DOM-seletores/script.js
@@ -1,3 +1,5 @@
+// getElementById
+
 /*
   1. Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
 
@@ -5,9 +7,6 @@
   3. Por fim, recupere o subtítulo e altere-o também.
 
 */
-
-
-
 
 const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
@@ -18,3 +17,5 @@ const paragraph2 = document.getElementById("second-paragraph");
 paragraph2.style.color = "yellow";
 
 const subtitleStyle = document.getElementById("subtitle").innerText = "Um dos melhores animes do mundo!!";
+
+console.log(document.getElementById("paragraph"));
---
+++ Exercicios/modulo-01-fundamentos-de-desenv-web/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-JavaScript-DOM-seletores/script4.js
@@ -0,0 +1,4 @@
+// querySelector
+
+console.log(document.querySelector('section'));
+console.log(document.querySelector('p'));
