d2da777ae192345e611db7aa34d4410cf25f7ee5
Marylange Leal <marylangess@gmail.com>
2022-02-12 20:03:09 -0300

    exercício 1

---
+++ Exercicios/modulo-01-fundamentos-de-desenv-web/bloco-04-introducao-a-javascript-logica-de-programacao/dia-01-javaScript-primeiros-passos/exercicio-do-dia/script1.js
@@ -0,0 +1,27 @@
+let a = 30;
+let b = 10;
+
+function adicao (a, b) {
+  return a + b;
+}
+console.log(adicao(a, b));
+
+function subtracao (a, b) {
+  return a - b;
+}
+console.log(subtracao(a, b));
+
+function multiplicacao (a, b) {
+  return a * b;
+}
+console.log(multiplicacao(a, b));
+
+function divisao (a, b) {
+  return a / b;
+}
+console.log(divisao(a, b));
+
+function modulo (a, b) {
+  return a % b;
+}
+console.log(modulo(a, b));
