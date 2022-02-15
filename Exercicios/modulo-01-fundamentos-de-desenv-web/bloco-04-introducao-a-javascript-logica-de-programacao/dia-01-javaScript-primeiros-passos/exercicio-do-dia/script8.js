954ea15262c076f63d3b2941bd4810f1e416a2ab
Marylange Leal <marylangess@gmail.com>
2022-02-15 10:11:16 -0300

    requisito 8

---
+++ Exercicios/modulo-01-fundamentos-de-desenv-web/bloco-04-introducao-a-javascript-logica-de-programacao/dia-01-javaScript-primeiros-passos/exercicio-do-dia/script8.js
@@ -0,0 +1,9 @@
+let numero1 = 33;
+let numero2 = 23;
+let numero3 = 13;
+
+if (numero1 % 2 !== 1 && numero2 % 2 !== 1 && numero3 % 2 !== 1) {
+  console.log(true);
+} else {
+  console.log(false);
+};
